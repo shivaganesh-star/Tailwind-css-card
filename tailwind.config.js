03f51ac9437d13c3adf7a2a975fed1ffce42a9c1
shivaganesh-star <136152992+shivaganesh-star@users.noreply.github.com>
2023-10-16 01:06:00 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {      colors: {
+      'regal-blue': '#243c5a',
+    },
+},
+  },
+  plugins: [],
+}
+
